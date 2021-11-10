7360ec2b932ae882661c6d79118f1a300d7bd8f7
Sanket A Darwante <sanketdarwante6@gmail.com>
2021-11-10 14:53:58 -0500

    Adding demo component and routing

---
+++ src/app/demo/demo.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DemoRoutingModule } from './demo-routing.module';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { MaterialModule } from '../shared/material.module';
+
+@NgModule({
+  declarations: [
+    ButtonsComponent
+  ],
+  imports: [
+    CommonModule,
+    DemoRoutingModule,
+    MaterialModule
+  ]
+})
+export class DemoModule { }
