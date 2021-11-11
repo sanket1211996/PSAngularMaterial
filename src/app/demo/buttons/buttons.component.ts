8193a568ea2864230d786fe394b2a123215c0f1a
Sanket A Darwante <sanketdarwante6@gmail.com>
2021-11-11 14:44:20 -0500

    Added demo module for material and flexbox demo.

--- src/app/demo/buttons/buttons.component.ts
+++ src/app/demo/buttons/buttons.component.ts
@@ -7,7 +7,6 @@ import { Component, OnInit } from '@angular/core';
         <mat-icon>face</mat-icon>
         Click Me!
       </button>
-
       <mat-checkbox>Check me!</mat-checkbox>
   `,
   styles: [
--- src/app/demo/demo-routing.module.ts
+++ src/app/demo/demo-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { FlexboxComponent } from './flexbox/flexbox.component';
 
 const routes: Routes = [
-  { path:'buttons', component: ButtonsComponent}
+  { path:'buttons', component: ButtonsComponent},
+  { path:'flexbox', component: FlexboxComponent},
+  //Redirect default base route to buttons
+  { path:'**', redirectTo:'buttons'}
 ];
 
 @NgModule({
