83c8fa9f2317eee978bb9050e1fd3314ef91a07b
Sanket A Darwante <sanketdarwante6@gmail.com>
2021-11-11 14:46:21 -0500

    Added contact manager module and forChild routing.

---
+++ src/app/contact-manager/contact-manager.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactManagerAppComponent } from './contact-manager-app.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path:'', component:ContactManagerAppComponent,
+    children: [
+      { path:'', component:MainContentComponent }
+    ]
+  },
+  { path:'**', redirectTo:'contactmanager'}
+
+]
+
+@NgModule({
+  declarations: [
+    ContactManagerAppComponent,
+    ToolbarComponent,
+    SidenavComponent,
+    MainContentComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    // forChild is used for other component routing apart forRoot in app module.
+    RouterModule.forChild(routes)
+  ]
+})
+export class ContactManagerModule { }
