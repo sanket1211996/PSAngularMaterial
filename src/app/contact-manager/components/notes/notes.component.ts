71ef8337a9ba998e4469b3729a090ae50e0ab753
Sanket A Darwante <sanketdarwante6@gmail.com>
2021-11-29 15:53:35 -0500

    Added table sorting

--- src/app/contact-manager/components/notes/notes.component.ts
+++ src/app/contact-manager/components/notes/notes.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Note } from '../../models/note';
 
@@ -16,15 +17,24 @@ export class NotesComponent implements OnInit, AfterViewInit {
   @ViewChild(MatPaginator)
   paginator!: MatPaginator;
 
+  @ViewChild(MatSort)
+  sort!: MatSort;
+
   @Input() notes!: Note[];
 
   constructor() { }
   ngAfterViewInit(): void {
     this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
 
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource<Note>(this.notes);
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
