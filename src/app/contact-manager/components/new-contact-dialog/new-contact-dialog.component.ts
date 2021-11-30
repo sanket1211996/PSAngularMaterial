29878f4e625b082436ef05b7ff068acc8b274087
Sanket A Darwante <sanketdarwante6@gmail.com>
2021-11-30 14:55:59 -0500

    Scafolding angular forms

--- src/app/contact-manager/components/new-contact-dialog/new-contact-dialog.component.ts
+++ src/app/contact-manager/components/new-contact-dialog/new-contact-dialog.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-new-contact-dialog',
@@ -7,17 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewContactDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialogRef: MatDialogRef<NewContactDialogComponent>) { }
+  avatars = [
+    'svg-1',
+    'svg-2',
+    'svg-3',
+    'svg-4',
+  ];
+  user!: User;
 
   ngOnInit(): void {
+    this.user = new User();
   }
 
   save(): void {
-
+    this.dialogRef.close(this.user);
   }
 
   dismiss(): void {
-
+    this.dialogRef.close(null);
   }
 
 }
